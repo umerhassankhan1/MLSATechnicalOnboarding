fae21dd2ac0c548057dbef3bf7df6fd942d143c4
Umer Hassan Khan <umerhassan113996@gmail.com>
2023-09-08 17:38:13 +0500

    another commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,12 +31,11 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "SQL",
+  "AWS",
+  "Azure",
+  "Databricks",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about data driven solutions. By leveraging my DE experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
