a7ed389af7f299c20648e022eff35a78c9018405
Umer Hassan Khan <umerhassan113996@gmail.com>
2023-09-08 16:38:14 +0500

    my new commit

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Databricks DE Project",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "",
+    url: "https://github.com/umerhassankhan1/Databricks-DE-project.git",
   },
   {
-    title: "Web Development for Beginners",
+    title: "DE Youtube Analysis Project",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "",
+    url: "https://github.com/umerhassankhan1/DE-youtube-analysis-project.git",
   },
   {
-    title: "My Resume Site",
+    title: "DE ETL Pipeline PostegreSQL Project",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "",
+    url: "https://github.com/umerhassankhan1/DE-project-etl-python-postgreSql.git",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Bytewise Fellowship in Data Engineering",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "",
+    url: "https://github.com/umerhassankhan1/bytewise_fellowship_DE.git",
   },
 ];
 
